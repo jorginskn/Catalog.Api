041a993a7f3afb016cef458698df334c946ea9b2
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-09-10 10:37:47 -0300

    feat:  CAT-01 => Criação de estrutura

---
+++ Catalogo.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalogo.Domain.Validation;
+ 
+namespace Catalogo.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public Category(string name, string imageUrl)
+        {
+            ValidateDomain(name, imageUrl);
+         }
+    }
+}
